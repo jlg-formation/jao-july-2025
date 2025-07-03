82b7e0b41d30da97338d849ffdb24736f6483394
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-07-03 10:16:42 +0100

    feat: added offset on angle

--- front/main.js
+++ front/main.js
@@ -4,6 +4,7 @@ const r0 = 1;
 const cx = 50;
 const cy = 50;
 const r = 45;
+const offset = -Math.PI / 2;
 
 const samples = 10;
 
@@ -11,7 +12,7 @@ const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
 
 for (let i = 0; i < samples; i++) {
-  const angle = (i * (2 * Math.PI)) / samples;
+  const angle = (i * (2 * Math.PI)) / samples + offset;
   const x = cx + r * Math.cos(angle);
   const y = cy + r * Math.sin(angle);
 
