df22196c1d750ebc5df4c4e27a21c92bef6347fc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-07-03 10:13:21 +0100

    feat: points on circle

--- front/main.js
+++ front/main.js
@@ -1,6 +1,9 @@
 console.log("start");
 
 const r0 = 1;
+const cx = 50;
+const cy = 50;
+const r = 45;
 
 const samples = 10;
 
@@ -8,8 +11,9 @@ const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
 
 for (let i = 0; i < samples; i++) {
-  const x = 23;
-  const y = 45;
+  const angle = (i * (2 * Math.PI)) / samples;
+  const x = cx + r * Math.cos(angle);
+  const y = cy + r * Math.sin(angle);
 
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x);
