76e7c8af9925e79d35f91168264c8c8fd020fa53
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-07-03 12:46:01 +0100

    refactor: use es6 module

--- front/constant.js
+++ front/constant.js
@@ -1,25 +1,8 @@
-(function () {
-  "use strict";
-  const r0 = 0.5;
-  const cx = 50;
-  const cy = 50;
-  const r = 45;
-  const offset = -Math.PI / 2;
-  const svgns = "http://www.w3.org/2000/svg";
-  const container = document.querySelector("g.samples");
-  const lineContainer = document.querySelector("g.lines");
-
-  const a = 12;
-
-  console.log("this: ", this);
-  globalThis.$multivizConstant = {
-    r0,
-    cx,
-    cy,
-    r,
-    offset,
-    svgns,
-    container,
-    lineContainer,
-  };
-})();
+export const r0 = 0.5;
+export const cx = 50;
+export const cy = 50;
+export const r = 45;
+export const offset = -Math.PI / 2;
+export const svgns = "http://www.w3.org/2000/svg";
+export const container = document.querySelector("g.samples");
+export const lineContainer = document.querySelector("g.lines");
