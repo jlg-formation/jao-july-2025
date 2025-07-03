ebc758f04a1cbc7b46221e94d8bcec24876e2e34
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-07-03 10:00:02 +0100

    feat: added circle with js

--- front/main.js
+++ front/main.js
@@ -1,7 +1,19 @@
 console.log("start");
 
+const r0 = 1;
+
 const samples = 10;
 
+const svgns = "http://www.w3.org/2000/svg";
+const container = document.querySelector("g.samples");
+
 for (let i = 0; i < samples; i++) {
-  console.log("tutu", i, i, i, "toto", "tata");
+  const x = 23;
+  const y = 45;
+
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", x);
+  circle.setAttributeNS(null, "cy", y);
+  circle.setAttributeNS(null, "r", r0);
+  container.appendChild(circle);
 }
